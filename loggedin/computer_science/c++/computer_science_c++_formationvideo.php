a0a6f0b2802e6b2593eac282afeb64a92108416e
Ruchika Naik <110330980+ruchikanaik@users.noreply.github.com>
2024-06-10 00:55:40 +0530

    Add files via upload

---
+++ loggedin/computer_science/c++/computer_science_c++_formationvideo.php
@@ -0,0 +1,114 @@
+<?php
+  session_start();
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="../java/accordionstyle.css">
+<link rel="stylesheet" href="../../../style.css">
+<script src="https://kit.fontawesome.com/f6b369aa9b.js" crossorigin="anonymous"></script>
+   
+<title>Brainiacs</title>
+</head>
+
+ <body>
+   <div class="topnav" id="myTopnav">
+   <a href="../../../loggedin.php" class="active">Home</a>
+       <?php
+       if (isset($_SESSION['userId'])){
+         echo '<a href="../../profile.php" name="profile">Profile</a>
+               <a href="../../../includes/logout.inc.php" name="logout-submit">SIGN OUT</a>';
+       }
+        ?>
+      </div>
+ </header>
+
+ <nav aria-label="breadcrumb">
+  <ol class="breadcrumb" style="background:linear-gradient(to right,rgba(242, 120, 159, 1),rgba(98, 205, 255, 1))">
+    <li class="breadcrumb-item" ><a href="../../../loggedin.php" style="color:white;">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page"><a href="../../computer_science.php" style="color:white;font-size:bold;">Computer Science</a></li>
+    <li class="breadcrumb-item active" aria-current="page"><a href="../../computer_science_c++.php" style="color:white;">C++</a></li>
+    <li class="breadcrumb-item active" aria-current="page" style="color:white;">Course C++</li>
+
+  </ol>
+</nav>
+<!-- the actual content of this course ( Cours C++ )
+     liste des chapitres-->
+
+  <div class="accordion" id="accordionExample">
+	<!-- Détails chapitre 1 -->
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Chapter 1 : Introduction
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        <a href="actualvideocontent_formationvideo.php?video=1">&#10170  Introduction</a><br>
+        <a href="actualvideocontent_formationvideo.php?video=2">&#10170  Basic Structures</a><br>
+        <a href="actualvideocontent_formationvideo.php?video=3">&#10170  Variables and Comments</a><br>
+        <a href="actualvideocontent_formationvideo.php?video=5">&#10170  C++ Reference Variables & Typecasting</a><br>
+      </div>
+    </div>
+  </div>
+  <!-- Détails chapitre 2 -->
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Chapter 2 : Manipulation in C++
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+        <a href="actualvideocontent_formationvideo.php?video=4">&#10170  Constants and Operators</a><br>
+        <a href="actualvideocontent_formationvideo.php?video=7">&#10170  Control Structure</a><br>
+        <a href="actualvideocontent_formationvideo.php?video=8">&#10170  Loop Structure</a><br>
+        <a href="actualvideocontent_formationvideo.php?video=9">&#10170  Pointers</a><br>
+      </div>
+    </div>
+  </div>
+  <!-- Détails chapitre 3 -->
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Chapter 3 : Programmation procedures
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        <a href="actualvideocontent_formationvideo.php?video=6">&#10170  Functions</a><br>
+        </div>
+    </div>
+  </div>
+  <!-- Détails chapitre 4 -->
+  <div class="card">
+    <div class="card-header" id="headingFour">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+           Chapter 4 : Object Oriented Programming
+        </button>
+      </h2>
+    </div>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+      <div class="card-body">
+        <a href="actualvideocontent_formationvideo.php?video=10">&#10170  Recursion & Recursive Functions</a><br>
+        <a href="actualvideocontent_formationvideo.php?video=11">&#10170  Function Overloading</a><br>
+        <a href="actualvideocontent_formationvideo.php?video=12">&#10170  Memory Allocation</a><br>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require "../../../footer.php" ?>
